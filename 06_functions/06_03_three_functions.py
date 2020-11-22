36b9641b517884b493cb7ec5c1f7dd49d45bead2
Martijn Nieuwland <martijn@nieuwland.mn>
2020-11-22 18:19:33 +0100

    mod 3 functions

--- 06_functions/06_03_three_functions.py
+++ 06_functions/06_03_three_functions.py
@@ -4,52 +4,23 @@ at least one other function and use the return value to do something.
 
 """
 
-# userAge = int(input("What age are you? "))
-#
-#
-# # Year of birth
-#
-# def birth_year(userAge):
-#     days = days_lived(userAge)
-#     return 2020 - (days / 365)
-#
-#
-# # days lived
-#
-# def days_lived(userAge):
-#     yearofbirth = birth_year(userAge)
-#     return (2020 - yearofbirth) * 365
-#
-#
-# # Leaps taken
-#
-# def leaps():
-#     years = days_lived(userAge)
-#     return years / 4
-#
-#
-# print(birth_year(userAge))
-# print(days_lived(userAge))
-# print(leaps())
 
-
-def takeanumber():
+def take_a_number():
     return input("Give me a number ")
 
 
 def double():
-    return takeanumber() * 2
+    return take_a_number() * 2
 
 
-def doubledouble():
+def double_double():
     return double() * 2
 
 
-def doubledoubledouble():
-    return doubledouble() * 2
-
+def double_double_double():
+    return double_double() * 2
 
 
 print(double())
-print(doubledouble())
-print(doubledoubledouble())
+print(double_double())
+print(double_double_double())
